fa9514f73077c6d2a790d187a224c55bd7687f74
aumetov <aumetov7@gmail.com>
2023-12-17 20:32:04 +0600

    Initial Commit

---
+++ EMSwiftUI/EMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMSwiftUIApp.swift
+//  EMSwiftUI
+//
+//  Created by Akbar Umetov on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
